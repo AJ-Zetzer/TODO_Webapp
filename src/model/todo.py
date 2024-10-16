bbaedec8f626ad8f95048c10f603f2547850e9ff
Alan-Zetzer <ajzetzer@gmail.com>
2024-10-16 13:54:53 -0600

    Web 1.0 App

---
+++ src/model/todo.py
@@ -0,0 +1,46 @@
+from peewee import *
+import logging
+
+db = SqliteDatabase('todos.db')
+
+logger = logging.getLogger('peewee')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+
+class Todo(Model):
+    text = CharField()
+    completed = BooleanField()
+    order = IntegerField(null=True)
+
+    def toggle_completed(self):
+        self.completed = not self.completed
+
+    @classmethod
+    def all(cls, view, search=None):
+        select = Todo.select()
+        if view == "active":
+            select = select.where(Todo.completed == False)
+        if view == "complete":
+            select = select.where(Todo.completed == True)
+
+        if search:
+            select = select.where(Todo.text.ilike("%" + search + "%"))
+
+        return select.order_by(Todo.order)
+
+    @classmethod
+    def find(cls, todo_id):
+        return Todo.get(Todo.id == todo_id)
+
+    @classmethod
+    def reorder(cls, id_list):
+        i = 0
+        for tid in id_list:
+            todo = Todo.find(int(tid))
+            todo.order = i
+            i += 1
+            todo.save()
+
+    class Meta:
+        database = db
